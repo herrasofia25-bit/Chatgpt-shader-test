ffd48267d77ac7b748f4572b7c9834d7dc34761e
herrasofia25-bit <herrasofia25@gmail.com>
2026-07-22 08:55:29 +0700

    Update common.glsl

--- shaders/lib/common.glsl
+++ shaders/lib/common.glsl
@@ -1,1 +1,11 @@
+#ifndef COMMON_GLSL
+#define COMMON_GLSL
 
+const vec3 SUN_COLOR = vec3(1.0, 0.95, 0.85);
+const vec3 SKY_COLOR = vec3(0.55, 0.72, 1.0);
+
+float saturate(float x){
+    return clamp(x,0.0,1.0);
+}
+
+#endif
